2049d5915cd2de05ddc223d691496e4d00fd6078
thebriando <thebriando@gmail.com>
2020-01-25 23:17:11 -0800

    switch to uncontrolled carousel

--- src/components/WithLibrariesCarousel/WithLibrariesCarousel.js
+++ src/components/WithLibrariesCarousel/WithLibrariesCarousel.js
@@ -1,47 +1,10 @@
 import React, { Component } from "react";
-import { Container, Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from "reactstrap";
+import { Container, UncontrolledCarousel } from "reactstrap";
 import "./WithLibrariesCarousel.css";
-export class WithLibrariesCarousel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeIndex: 0
-    };
-  }
-  next = () => {
-    const currentIndex = this.state.activeIndex;
-    this.setState({ activeIndex: currentIndex === this.props.items.length - 1 ? 0 : currentIndex + 1 });
-  };
-  previous = () => {
-    const currentIndex = this.state.activeIndex;
-    this.setState({ activeIndex: currentIndex === 0 ? this.props.items.length - 1 : currentIndex - 1 });
-  };
-  goToIndex = index => {
-    this.setState({ activeIndex: index });
-  };
-  render() {
-    const { activeIndex } = this.state;
-    return (
-      <Container>
-        <Carousel
-          className="carousel"
-          activeIndex={activeIndex}
-          next={this.next}
-          previous={this.previous}
-          interval={false}
-        >
-          <CarouselIndicators items={this.props.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-          {this.props.items.map(item => {
-            return (
-              <CarouselItem key={item.src}>
-                <img className="carousel-image" src={item.src} alt={item.altText} />
-              </CarouselItem>
-            );
-          })}
-          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-        </Carousel>
-      </Container>
-    );
-  }
-}
+export const WithLibrariesCarousel = () => {
+  return (
+    <Container>
+      <UncontrolledCarousel items={this.props.items} />
+    </Container>
+  );
+};
