0d7630c10c0089f862aab44b645d087a78c779dc
thebriando <thebriando@gmail.com>
2020-01-25 22:48:14 -0800

    styling :)

--- src/components/WithLibrariesCarousel/WithLibrariesCarousel.js
+++ src/components/WithLibrariesCarousel/WithLibrariesCarousel.js
@@ -1,54 +1,44 @@
 import React, { Component } from "react";
 import { Container, Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from "reactstrap";
-import "./WithLibrariesCarousel.css"
+import "./WithLibrariesCarousel.css";
 export class WithLibrariesCarousel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeIndex: 0,
-      slides: [],
-      animating: false
+      activeIndex: 0
     };
   }
-  componentDidMount() {
-    const slides = this.props.items.map(item => {
-      return (
-        <CarouselItem
-          // onExiting={() => this.setAnimating(true)}
-          // onExited={() => this.setAnimating(false)}
-          key={item.src}
-        >
-          <img className="carousel-image" src={item.src} alt={item.altText} />
-          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
-        </CarouselItem>
-      );
-    });
-    this.setState({ slides: slides });
-  }
-  setAnimating = bool => {
-    this.setState({ animating: bool });
-  };
   next = () => {
-    if (this.state.animating) return;
     const currentIndex = this.state.activeIndex;
     this.setState({ activeIndex: currentIndex === this.props.items.length - 1 ? 0 : currentIndex + 1 });
   };
   previous = () => {
-    if (this.state.animating) return;
     const currentIndex = this.state.activeIndex;
     this.setState({ activeIndex: currentIndex === 0 ? this.props.items.length - 1 : currentIndex - 1 });
   };
   goToIndex = index => {
-    if (this.state.animating) return;
     this.setState({ activeIndex: index });
   };
   render() {
-    const { activeIndex, slides } = this.state;
+    const { activeIndex } = this.state;
     return (
-      <Container className="carousel-container">
-        <Carousel className="carousel" activeIndex={activeIndex} next={this.next} previous={this.previous} interval={false} >
+      <Container>
+        <Carousel
+          className="carousel"
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+          interval={false}
+        >
           <CarouselIndicators items={this.props.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-          {slides}
+          {this.props.items.map(item => {
+            return (
+              <CarouselItem key={item.src}>
+                <img className="carousel-image" src={item.src} alt={item.altText} />
+                <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+              </CarouselItem>
+            );
+          })}
           <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
           <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
         </Carousel>
--- src/components/WithoutLibrariesCarousel/WithoutLibrariesCarousel.js
+++ src/components/WithoutLibrariesCarousel/WithoutLibrariesCarousel.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
 import "./WithoutLibrariesCarousel.css";
 
 export class WithoutLibrariesCarousel extends Component {
@@ -11,49 +10,44 @@ export class WithoutLibrariesCarousel extends Component {
     };
   }
   next = () => {
-    this.setState({ transition: false });
     const current = this.state.current;
-    this.setState({ current: current === this.props.items.length - 1 ? 0 : current + 1, transition: true });
+    this.setState({ current: current === this.props.items.length - 1 ? 0 : current + 1 });
   };
   prev = () => {
-    this.setState({ transition: false });
     const current = this.state.current;
-    this.setState({ current: current === 0 ? this.props.items.length - 1 : current - 1, transition: true });
+    this.setState({ current: current === 0 ? this.props.items.length - 1 : current - 1 });
   };
   change = index => {
-    this.setState({ transition: false });
     this.setState({ current: index, transition: true });
   };
   render() {
-    const { current, transition } = this.state;
-    let imgClass = "wo-libraries-image";
-    transition && (imgClass += " transition");
+    const { current } = this.state;
     return (
-      <Container>
-        <img
-          key={this.props.items[current].src}
-          src={this.props.items[current].src}
-          alt={this.props.items[current].alt}
-          className={imgClass}
-        />
-        <div className="wo-libraries-navigation">
-          <button className="carousel-button prev" onClick={this.prev}>
+      <div className="container wo-libraries-container">
+        <div className="wo-libraries-carousel">
+          <button className="prev-btn" onClick={this.prev}>
             &lt;
           </button>
-          <button className="carousel-button next" onClick={this.next}>
+          <img
+            key={this.props.items[current].src}
+            src={this.props.items[current].src}
+            alt={this.props.items[current].alt}
+            className="wo-libraries-image transition"
+          />
+          <button className="next-btn" onClick={this.next}>
             &gt;
           </button>
         </div>
         <div className="wo-libraries-selectors">
           {this.props.items.map((item, index) => {
-            let btnClass = "carousel-button selector";
+            let btnClass = "carousel-selector";
             if (index === current) {
               btnClass += " current";
             }
             return <button key={item.alt} className={btnClass} onClick={() => this.change(index)}></button>;
           })}
         </div>
-      </Container>
+      </div>
     );
   }
 }
